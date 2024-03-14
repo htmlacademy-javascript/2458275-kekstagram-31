a43ffce800024e587773a59ae27ad819f02282f2
KseniiaPan <panovskaia.ksenia74@gmail.com>
2024-03-14 18:59:56 +0300

    Выполнено разделение на модули

---
+++ js/createItems.js
@@ -0,0 +1,44 @@
+import {getRandomInteger, getUniqueRandomInteger, getRandomArrayElement} from './utils.js';
+import {COMMENTS, NAMES, DESCRIPTIONS} from './mockData.js';
+
+const PHOTOS_TOTAL = 25;
+
+const LIKES_NUM = {
+  min: 15,
+  max: 200,
+};
+const COMMENTS_NUM = {
+  min: 0,
+  max: 30,
+};
+const AVATAR_NUM = {
+  min: 1,
+  max: 6,
+};
+
+const createComment = () => () => {
+  const comment = {};
+  comment.id = getUniqueRandomInteger(1, COMMENTS_NUM.max * PHOTOS_TOTAL);
+  comment.avatar = `img/avatar-${getRandomInteger(AVATAR_NUM.min, AVATAR_NUM.max)}.svg`;
+  comment.message = getRandomArrayElement(COMMENTS);
+  comment.name = getRandomArrayElement(NAMES);
+  return comment;
+};
+
+const createPhoto = () => {
+  let id = 1;
+  return () => {
+    const photo = {};
+    photo.id = id;
+    photo.url = `photos/${id}.jpg`;
+    photo.description = getRandomArrayElement(DESCRIPTIONS);
+    photo.likes = getRandomInteger(LIKES_NUM.min, LIKES_NUM.max);
+    photo.comments = Array.from({length: getRandomInteger(0, COMMENTS_NUM.max)}, createComment());
+    id++;
+    return photo;
+  };
+};
+
+const photosArray = Array.from({length: PHOTOS_TOTAL}, createPhoto());
+
+export {photosArray};
