a905763af850e0053a80a8ea8ec5fcb4c3dcea1d
Keks <keks@htmlacademy.ru>
2024-03-30 15:23:30 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,7 @@
+import {createThumbnails, userPictures} from './create-thumbnails.js';
+import {onThumbnailClick} from './open-fullsize-photos.js';
+
+createThumbnails(userPictures);
+onThumbnailClick(userPictures);
+
+
