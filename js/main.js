6bfbdce636c9c99cfddb2f62ad915a0fec20f366
KseniiaPan <panovskaia.ksenia74@gmail.com>
2024-04-03 13:09:07 +0300

    Добавлена возможность редактирования масштаба изображения

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import {createThumbnails, userPictures} from './create-thumbnails.js';
 import {showBigPictureInfo} from './open-fullsize-photos.js';
 import './photo-upload-form.js';
+import './photo-scale-control.js';
 
 
 createThumbnails(userPictures);
