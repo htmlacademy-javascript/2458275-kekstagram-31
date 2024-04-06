b376e589fb94f0b2d481b1af08058b8c03879df9
KseniiaPan <panovskaia.ksenia74@gmail.com>
2024-04-06 13:53:27 +0300

    внсеены исправления в функции для валидации

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,7 +1,7 @@
 const MAX_HASHTAGS_AMOUNT = 5;
 const MAX_HASHTAG_LENGTH = 20;
 const MAX_DESCRIPTION_LENGTH = 140;
-const VALID_HASHTAG_SYMBOLS = /^#[a-zа-яё0-9]{1,19}$/i;
+const VALID_HASHTAG_SYMBOLS = /[a-zа-яё0-9#]{1,19}$/i;
 
 let errorMessage = '';
 const generateErrorMessage = () => errorMessage;
@@ -19,27 +19,27 @@ const isHashtagValid = (value) => {
 
   const rules = [
     {
-      check: hashtagsArray.some((item) => item[0] !== '#'),
+      check: hashtagsArray.every((item) => item[0] !== '#'),
       error:  'Хэштег должен начинаться с символа # (решётка)',
     },
 
     {
-      check: hashtagsArray.some((item) => !VALID_HASHTAG_SYMBOLS.test(item)),
+      check: hashtagsArray.every((item) => !VALID_HASHTAG_SYMBOLS.test(item)),
       error: 'Пробелы, спецсимволы, символы пунктуации, эмодзи недопустимы. Хэштег должен состоять только из букв и чисел',
     },
 
     {
-      check: hashtagsArray.some((item) => item === '#'),
+      check: hashtagsArray.every((item) => item === '#'),
       error: 'Хэштег не может состоять только из одной решётки',
     },
 
     {
-      check: hashtagsArray.some((item) => item.length > MAX_HASHTAG_LENGTH),
+      check: hashtagsArray.every((item) => item.length > MAX_HASHTAG_LENGTH),
       error: `Максимальная длина одного хэштега ${MAX_HASHTAG_LENGTH} символов, включая решётку`,
     },
 
     {
-      check: hashtagsArray.some((item) => item.slice(1).includes('#')),
+      check: hashtagsArray.every((item) => item.slice(1).includes('#')),
       error: 'Хэштеги разделяются пробелами',
     },
 
